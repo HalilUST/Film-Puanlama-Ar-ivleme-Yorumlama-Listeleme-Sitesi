ebef72c27783638a6db23fce79dceba2613c7adf
Arda Burak Yigit <yigitardaburak17@gmail.com>
2026-05-02 18:41:53 +0300

    Login ve Register HTML formlari ile GET/POST altyapisi eklendi

--- src/app/views.py
+++ src/app/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 
 views = Blueprint('views', __name__)
 
@@ -6,6 +6,29 @@ views = Blueprint('views', __name__)
 def home():
     return "<h1>Film Oneri Sitemize Hosgeldiniz! - Arda</h1>"
 
-@views.route('/login')
+# methods=['GET', 'POST'] diyerek hem sayfayı açmayı hem de butona basmayı kabul ediyoruz
+@views.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<h1>Burasi Giris Yapma Sayfasi Olacak</h1>"
+    if request.method == 'POST':
+        # Butona basıldığında formdaki (name="..." olan) bilgileri çekiyoruz
+        kullanici_adi = request.form.get('username')
+        sifre = request.form.get('password')
+        
+        # Şimdilik Burak veritabanını kurmadığı için veriyi sadece ekrana yazdıralım
+        return f"<h1>Gelen Bilgiler -> Kullanici: {kullanici_adi}, Sifre: {sifre}</h1>"
+    
+    # Eğer butona basılmadıysa (sadece sayfa açıldıysa) formu göster
+    return render_template("login.html")
+
+@views.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        kullanici_adi = request.form.get('username')
+        sifre = request.form.get('password')
+        return f"<h1>KAYIT BASARILI -> Kullanici: {kullanici_adi}, Sifre: {sifre}</h1>"
+        
+    return render_template("register.html")
+
+@views.route('/film/<int:id>')
+def film_detay(id):
+    return f"<h1>Burasi {id} numarali filmin sayfasi. Detaylar gelecek.</h1>"
